66b6bbb0c5599870228304d5029ff5fe7d00fb03
mamun sj <mamunsj2616@gmail.com>
2023-01-09 03:14:52 +0600

    status code added

--- html css/timeline/Module/server.js
+++ html css/timeline/Module/server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const port = 3000;
 const hostname = '127.0.0.1';
 const myServer = http.createServer((req, res)=>{
-    res.end("<h1>My first ever made server is running successfully</h1>");
+    // res.end("<h1>My first ever made server is running successfully</h1>");
+    res.writeHead(202,{'Content-Type':'text/html'});
+    res.write("<h2>Yess You server works well</h2>");
+    res.end();
 })
 
 myServer.listen( port, hostname, ()=>{
